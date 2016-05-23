36b56518124ad8129d90b807870c9972817245df
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2016-05-23 02:11:43 -0700

    ya jala este login

--- DB/connect.php
+++ DB/connect.php
@@ -1,25 +1,17 @@
 <?php
-	class DB_Connect {
-		//Constructor
-		function __construct(){
-			$this->connect(); //Se conecta a la base de datos.
-		}
-		//Destructor
-		function __destruct(){
-			$this->close(); //Se cierra la conexión a la base de datos.
-		}
-		//Método para conectarse a la base de datos.
-		function connect(){
-			//Es una clase de import. Consigue las variables de db_config.php
-			require_once __DIR__ . '/config.php';
-			//Conectando a la base de datos.
-			$con = new mysqli(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
-			//Se regresa el cursor de la conexión.
-			return $con;
-		}
-		//Método para desconectarse de la base de datos.
-		function close(){
-			mysqli_close($this->connect());
-		}
-	}
-?>
+
+
+function dbConnect (){
+
+    //  Conecta con el servidor mysql
+  $link = mysqli_connect("localhost","root","","clinica");
+  if(!$link) {
+
+    // Si no  conecta, muestra un error y termina la ejecucion
+    die('No  se pudo conectar con la base de datos:' . mysql_error());
+ }
+
+ 
+return $link;
+}
+?> 
